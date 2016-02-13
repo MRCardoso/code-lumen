3f63423dcac1d07b6f3c1bcf0cb3f91c301ff7d8
MRCardoso <cardozinhos.mrc@gmail.com>
2016-02-13 11:48:32 -0200

    apply instalation of the scripts by manage of the Bower and Gulp

--- resources/views/agenda/index.blade.php
+++ resources/views/agenda/index.blade.php
@@ -1,25 +1,12 @@
-<!DOCTYPE html>
-<html lang="pt-br">
-    <head>
-        <meta charset="UTF-8">
-        <title>Agenda</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="{{ url('/libs/bootstrap/dist/css/bootstrap.min.css') }}">
-        <script language="JavaScript" src="{{ url('/libs/jquery/dist/jquery.js') }}"></script>
-        <script language="JavaScript" src="{{ url('/libs/bootstrap/dist/js/bootstrap.min.js') }}"></script>
-        <script language="javascript" src="{{ url('/libs/bootbox/bootbox.js') }}"></script>
-    </head>
-    <body>
-        <div class="container">
-            <h3 class="page-header">
-                Welcome
-            </h3>
-            <div class="panel-body">
-                we are starting the development...
-            </div>
-        </div>
-    </body>
-</html>
+@extends('templates.layout')
+@section('content')
+    <h3 class="page-header">
+        Welcome
+    </h3>
+    <div class="panel-body">
+        we are starting the development...
+    </div>
+@endsection
 
 
 
